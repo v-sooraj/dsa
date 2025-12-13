0fd32deb49d92d62574f3611664864a777dd0840
SoorajV1995 <soorajv1995@gmail.com>
2025-12-13 18:44:59 +0530

    two-pointers: valid palindrome and middle of LL

---
+++ problems/arrays/two-pointers/src/easy/ValidPalindrome.java
@@ -0,0 +1,45 @@
+package easy;
+
+/**
+ * <h3> Valid Palindrome <h3>
+ * <p>
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ * <p>
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * */
+public class ValidPalindrome {
+
+    private static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters (case-insensitive)
+            if (Character.toLowerCase(s.charAt(left))
+                    != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(input));
+    }
+}
